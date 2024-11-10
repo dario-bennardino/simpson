8a55fcbc65a80c7a212bef987532098f291f6762
dario.bennardino@gmail.com <dario.bennardino@gmail.com>
2024-11-10 19:17:00 +0100

    [add] search

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,196 @@
-import Image from "next/image";
+"use client";
+import { useState, useEffect, useRef } from "react";
+import axios from "axios";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [beerName, setbeerName] = useState("");
+  const [beers, setbeers] = useState([]);
+  const [allbeers, setAllbeers] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [isClient, setIsClient] = useState(false); // Stato per verificare se è lato client
+  const suggestionsRef = useRef(null); // Riferimento per la lista dei suggerimenti
+  const inputRef = useRef(null); // Riferimento per l'input
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  // Imposta isClient su true dopo il montaggio del componente (lato client)
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  // Carica i dati dal server solo quando siamo sul client
+  useEffect(() => {
+    if (isClient) {
+      const fetchAllbeers = async () => {
+        setLoading(true);
+        try {
+          const response = await axios.get(
+            "https://api.sampleapis.com/beers/ale"
+          );
+          console.log(response.data);
+          setAllbeers(response.data);
+        } catch (error) {
+          console.error("Error fetching data", error);
+        } finally {
+          setLoading(false);
+        }
+      };
+
+      fetchAllbeers();
+    }
+  }, [isClient]);
+
+  // Funzione per gestire il cambio del campo di ricerca
+  const handleInputChange = (e) => {
+    const value = e.target.value;
+    setbeerName(value);
+
+    if (value.length > 0) {
+      const filteredSuggestions = allbeers.filter((beer) =>
+        beer.name.toLowerCase().includes(value.toLowerCase())
+      );
+      setSuggestions(filteredSuggestions);
+    } else {
+      setSuggestions([]);
+    }
+  };
+
+  // Funzione per selezionare un personaggio dalla lista
+  const handleSuggestionClick = (name) => {
+    setbeerName(name);
+    setSuggestions([]);
+    searchbeer(name);
+  };
+
+  // Funzione per fare la ricerca
+  const searchbeer = async (name) => {
+    console.log("Ricerca per: ", name);
+    if (!name) return;
+    setLoading(true);
+    setErrorMessage("");
+
+    try {
+      const response = await axios.get("https://api.sampleapis.com/beers/ale");
+      console.log(response.data);
+
+      const filteredbeers = response.data.filter((beer) =>
+        beer.name.toLowerCase().includes(name.toLowerCase())
+      );
+
+      if (filteredbeers.length > 0) {
+        setbeers(filteredbeers);
+      } else {
+        setbeers([]);
+        setErrorMessage("Nessun personaggio trovato con questo nome.");
+      }
+    } catch (error) {
+      setErrorMessage("Si è verificato un errore nella ricerca.");
+      console.error("Error fetching data", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // Funzione per gestire il click al di fuori dell'input e della lista dei suggerimenti
+  const handleOutsideClick = (e) => {
+    if (
+      inputRef.current &&
+      !inputRef.current.contains(e.target) &&
+      suggestionsRef.current &&
+      !suggestionsRef.current.contains(e.target)
+    ) {
+      setSuggestions([]); // Nasconde la lista dei suggerimenti
+    }
+  };
+
+  // Aggiungere l'evento al caricamento della pagina
+  useEffect(() => {
+    if (isClient) {
+      document.addEventListener("click", handleOutsideClick);
+      return () => {
+        document.removeEventListener("click", handleOutsideClick);
+      };
+    }
+  }, [isClient]);
+
+  if (!isClient) {
+    return null; // Non renderizzare nulla finché non siamo lato client
+  }
+
+  return (
+    <div className="container mx-auto mt-10 px-4">
+      <h1 className="text-white text-4xl text-center mb-8 font-bold">Beers</h1>
+
+      {/* Input per il nome del personaggio */}
+      <div className="relative flex justify-center mb-6">
+        <input
+          ref={inputRef} // Aggiungi il riferimento all'input
+          type="text"
+          className="border border-gray-300 rounded-lg p-3 w-1/2"
+          placeholder="Cerca un personaggio..."
+          value={beerName}
+          onChange={handleInputChange}
+        />
+        <button
+          onClick={() => searchbeer(beerName)}
+          disabled={loading}
+          className="ml-3 bg-blue-500 text-white p-3 rounded-lg hover:bg-blue-600 disabled:opacity-50"
+        >
+          {loading ? "Caricamento..." : "Cerca"}
+        </button>
+
+        {/* Lista dei suggerimenti */}
+        {suggestions.length > 0 && (
+          <div
+            ref={suggestionsRef} // Aggiungi il riferimento alla lista dei suggerimenti
+            className="absolute z-10 bg-white border border-gray-300 w-full max-h-60 overflow-y-auto mt-1 rounded-lg shadow-lg"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            {suggestions.map((beer) => (
+              <div
+                key={beer.id}
+                className="p-2 cursor-pointer hover:bg-gray-200"
+                onClick={() => handleSuggestionClick(beer.name)}
+              >
+                {beer.name}
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+
+      {/* Messaggio di errore */}
+      {errorMessage && (
+        <div className="text-red-500 text-center mb-4">{errorMessage}</div>
+      )}
+
+      {/* Risultati della ricerca */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-4">
+        {Array.isArray(beers) && beers.length > 0 ? (
+          beers.map((beer) => (
+            <div className="max-w-xs mx-auto" key={beer.id}>
+              <div className="bg-white shadow-lg rounded-lg overflow-hidden">
+                <div className="w-full h-48 bg-gray-200 flex items-center justify-center">
+                  <img src={beer.image} alt={beer.name} />
+                </div>
+                <div className="p-4">
+                  <h5 className="text-xl font-semibold">{beer.name}</h5>
+                  <p className="text-gray-600 mt-2">ID: {beer.id}</p>
+                  <p className="text-gray-600 mt-2">
+                    Nome normalizzato: {beer.normalized_name}
+                  </p>
+                  <p className="text-gray-600 mt-2">
+                    Genere: {beer.gender || "Non specificato"}
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))
+        ) : (
+          <p className="text-center text-gray-500">
+            Nessun personaggio da mostrare.
+          </p>
+        )}
+      </div>
     </div>
   );
 }
